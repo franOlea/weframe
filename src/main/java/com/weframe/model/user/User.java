3a0967dbc9f3aab1dd80fba7609d5bceaa417fdb
Francisco Olea <franciscom2.olea@gmail.com>
2016-09-07 14:27:04 -0300

    Adds junit, apache commons to pom and creates basic user class

---
+++ src/main/java/com/weframe/model/user/User.java
@@ -0,0 +1,74 @@
+package com.weframe.model.user;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String passwordSalt;
+
+    public User(final long id,
+                final String firstName,
+                final String lastName,
+                final String email,
+                final String password,
+                final String passwordSalt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.password = passwordSalt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
