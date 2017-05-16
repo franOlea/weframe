7fe239125f23c41799616697fa52315bd8f49c79
Francisco Olea <fran.aztk@gmail.com>
2017-05-16 00:08:00 -0300

    Adds thumbnail method to picture service abstract class

--- src/main/java/com/weframe/picture/service/PictureService.java
+++ src/main/java/com/weframe/picture/service/PictureService.java
@@ -3,8 +3,12 @@ package com.weframe.picture.service;
 import com.weframe.controllers.EmptyResultException;
 import com.weframe.picture.model.Picture;
 import com.weframe.picture.service.exception.InvalidPicturePersistenceException;
+import net.coobird.thumbnailator.Thumbnails;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 public abstract class PictureService {
 
@@ -26,5 +30,11 @@ public abstract class PictureService {
     public abstract Picture create(final File pictureFile, String uniqueName) throws InvalidPicturePersistenceException;
 
     public abstract void delete(final Long id) throws InvalidPicturePersistenceException;
+
+    protected BufferedImage createThumbnail(BufferedImage original, int width, int height) throws IOException {
+        return Thumbnails.of(original)
+                .size(width, height)
+                .asBufferedImage();
+    }
 
 }
