35f907ffe61620b0c39c64f50b8a0c293fb81e0c
Francisco Olea <fran.aztk@gmail.com>
2017-07-21 10:19:45 -0300

    Refactors backboard contorller

--- src/main/java/com/weframe/product/generic/controllers/BackBoardController.java
+++ src/main/java/com/weframe/product/generic/controllers/BackBoardController.java
@@ -2,7 +2,6 @@ package com.weframe.product.generic.controllers;
 
 import com.weframe.controllers.EmptyResultException;
 import com.weframe.controllers.ResponseGenerator;
-import com.weframe.controllers.errors.Error;
 import com.weframe.picture.model.Picture;
 import com.weframe.picture.service.PictureService;
 import com.weframe.picture.service.exception.InvalidPicturePersistenceException;
@@ -10,12 +9,10 @@ import com.weframe.product.generic.model.BackBoard;
 import com.weframe.product.generic.service.exception.InvalidGenericProductPersistenceException;
 import com.weframe.product.generic.service.impl.BackBoardService;
 import org.apache.log4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.Collections;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 @RestController
@@ -47,49 +44,15 @@ public class BackBoardController {
             if(uniqueName != null) {
                 return getBackBoardByUniqueName(uniqueName, isOriginalSize);
             }
-
             if(page < 0 || size < 0) {
-                Error error = new Error(
-                        "invalid-request",
-                        "The page and size parameters must be above zero."
-                );
-                return responseGenerator.generateErrorResponse(
-                        Collections.singleton(error),
-                        HttpStatus.UNPROCESSABLE_ENTITY
-                );
+                return responseGenerator.generatePageRequestErrorResponse();
             }
-
             Collection<BackBoard> backBoards = backBoardService.getAll(page, size);
-            backBoards.forEach(backBoard -> {
-                try {
-                    assignPictureUrl(backBoard, true);
-                } catch (InvalidPicturePersistenceException e) {
-                    logger.error(
-                            String.format(
-                                    "There has been an error while trying to generate the backboard's picture url " +
-                                            "for picture [%s]",
-                                    backBoard.getPicture().getImageKey()),
-                            e);
-                    backBoard.getPicture().setImageUrl("no-url");
-                }
-            });
+            assignPictureUrl(backBoards, !isOriginalSize);
             return responseGenerator.generateResponse(backBoards);
-        } catch (InvalidGenericProductPersistenceException e) {
-            logger.error(
-                    String.format(
-                            "There has been an error requesting backboards by page [%d] " +
-                                    "with size [%d]",
-                            page,
-                            size),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. " +
-                            "Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException
+                | InvalidPicturePersistenceException e) {
+            return handleUnexpectedError(e);
         } catch (EmptyResultException e) {
             return responseGenerator.generateEmptyResponse();
         }
@@ -107,21 +70,9 @@ public class BackBoardController {
             );
         } catch (EmptyResultException e) {
             return responseGenerator.generateEmptyResponse();
-        } catch (Exception e) {
-            logger.error(
-                    String.format(
-                            "There was an unexpected error trying to fetch a " +
-                                    "backboard by id [%d].",
-                            backBoardId
-                    ),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException
+                | InvalidPicturePersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
@@ -132,21 +83,9 @@ public class BackBoardController {
             return responseGenerator.generateResponse(backBoard);
         } catch (EmptyResultException e) {
             return responseGenerator.generateEmptyResponse();
-        } catch (Exception e) {
-            logger.error(
-                    String.format(
-                            "There was an unexpected error trying to fetch a " +
-                                    "backboard by uniqueName [%s].",
-                            backBoardUniqueName
-                    ),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException
+                | InvalidPicturePersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
@@ -164,22 +103,11 @@ public class BackBoardController {
             backBoard.setPicture(picture);
             backBoardService.persist(backBoard);
             return responseGenerator.generateOkResponse();
-        } catch(InvalidGenericProductPersistenceException |
-                InvalidPicturePersistenceException |
-                EmptyResultException e) {
-            logger.error(
-                    String.format(
-                            "There has been an error creating the backboard [%s]",
-                            backBoard.getUniqueName()),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. " +
-                            "Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (EmptyResultException e) {
+            return responseGenerator.generateEmptyResponse();
+        } catch (InvalidGenericProductPersistenceException
+                | InvalidPicturePersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
@@ -198,22 +126,11 @@ public class BackBoardController {
             backBoard.setPicture(picture);
             backBoardService.persist(backBoard);
             return responseGenerator.generateOkResponse();
-        } catch(InvalidGenericProductPersistenceException |
-                InvalidPicturePersistenceException |
-                EmptyResultException e) {
-            logger.error(
-                    String.format(
-                            "There has been an error creating the backboard [%s]",
-                            backBoard.getUniqueName()),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. " +
-                            "Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (EmptyResultException e) {
+            return responseGenerator.generateEmptyResponse();
+        } catch (InvalidGenericProductPersistenceException
+                | InvalidPicturePersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
@@ -223,20 +140,8 @@ public class BackBoardController {
             backBoardService.delete(backBoardId);
             logger.info("Deleted backboard [" + backBoardId + "].");
             return responseGenerator.generateOkResponse();
-        } catch (Exception e) {
-            logger.error(
-                    String.format(
-                            "There was an unexpected error trying to delete a backboard" +
-                                    " by id [%d].",
-                            backBoardId),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
@@ -244,6 +149,17 @@ public class BackBoardController {
         backBoard.getPicture().setImageUrl(
                 pictureService.getPictureUrl(backBoard.getPicture().getImageKey(), thumbnail)
         );
+    }
+
+    private void assignPictureUrl(final Collection<BackBoard> backBoards, final boolean thumbnail) throws InvalidPicturePersistenceException {
+        for(BackBoard backBoard : backBoards) {
+            assignPictureUrl(backBoard, thumbnail);
+        }
+    }
+
+    private ResponseEntity handleUnexpectedError(final Exception e) {
+        logger.error("There was an unexpected error while doing an operation on user pictures.", e);
+        return responseGenerator.generateInternalServerErrorResponse();
     }
     
 }
