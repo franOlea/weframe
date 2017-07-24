28724e335f0880e9fd3eb623291ef873ef23b811
Francisco Olea <fran.aztk@gmail.com>
2017-07-24 10:26:13 -0300

    Improves frame glass controller

--- src/main/java/com/weframe/product/generic/controllers/FrameGlassController.java
+++ src/main/java/com/weframe/product/generic/controllers/FrameGlassController.java
@@ -3,6 +3,7 @@ package com.weframe.product.generic.controllers;
 import com.weframe.controllers.EmptyResultException;
 import com.weframe.controllers.ResponseGenerator;
 import com.weframe.controllers.errors.Error;
+import com.weframe.picture.service.exception.InvalidPicturePersistenceException;
 import com.weframe.product.generic.model.FrameGlass;
 import com.weframe.product.generic.service.exception.InvalidGenericProductPersistenceException;
 import com.weframe.product.generic.service.impl.FrameGlassService;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.Collections;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
@@ -39,37 +41,12 @@ public class FrameGlassController {
             if(uniqueName != null) {
                 return getFrameGlassByUniqueName(uniqueName);
             }
-
             if(page < 0 || size < 0) {
-                Error error = new Error(
-                        "invalid-request",
-                        "The page and size parameters must be above zero."
-                );
-                return responseGenerator.generateErrorResponse(
-                        Collections.singleton(error),
-                        HttpStatus.UNPROCESSABLE_ENTITY
-                );
+                responseGenerator.generatePageRequestErrorResponse();
             }
-
-            return responseGenerator.generateResponse(
-                    frameGlassService.getAll(page, size)
-            );
+            return responseGenerator.generateResponse(frameGlassService.getAll(page, size));
         } catch (InvalidGenericProductPersistenceException e) {
-            logger.error(
-                    String.format(
-                            "There has been an error requesting backboards by page [%d] " +
-                                    "with size [%d]",
-                            page,
-                            size),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. " +
-                            "Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+            return handleUnexpectedError(e);
         } catch (EmptyResultException e) {
             return responseGenerator.generateEmptyResponse();
         }
@@ -78,52 +55,21 @@ public class FrameGlassController {
     @RequestMapping(value = "/{frameGlassId}", method = RequestMethod.GET)
     private ResponseEntity getFrameGlass(@PathVariable Long frameGlassId) {
         try {
-            FrameGlass frameGlass = frameGlassService.getById(frameGlassId);
-            return responseGenerator.generateResponse(
-                    frameGlassService.getById(frameGlassId)
-            );
+            return responseGenerator.generateResponse(frameGlassService.getById(frameGlassId));
         } catch (EmptyResultException e) {
             return responseGenerator.generateEmptyResponse();
-        } catch (Exception e) {
-            logger.error(
-                    String.format(
-                            "There was an unexpected error trying to fetch a " +
-                                    "frameglass by id [%d].",
-                            frameGlassId
-                    ),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
     private ResponseEntity getFrameGlassByUniqueName(String frameGlassUniqueName) {
         try {
-            return responseGenerator.generateResponse(
-                    frameGlassService.getByUniqueName(frameGlassUniqueName)
-            );
+            return responseGenerator.generateResponse(frameGlassService.getByUniqueName(frameGlassUniqueName));
         } catch (EmptyResultException e) {
             return responseGenerator.generateEmptyResponse();
         } catch (Exception e) {
-            logger.error(
-                    String.format(
-                            "There was an unexpected error trying to fetch a " +
-                                    "frameglass by uniqueName [%s].",
-                            frameGlassUniqueName
-                    ),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+            return handleUnexpectedError(e);
         }
     }
 
@@ -137,20 +83,8 @@ public class FrameGlassController {
             }
             frameGlassService.persist(frameGlass);
             return responseGenerator.generateOkResponse();
-        } catch(InvalidGenericProductPersistenceException e) {
-            logger.error(
-                    String.format(
-                            "There has been an error creating the frameglass [%s]",
-                            frameGlass.getUniqueName()),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. " +
-                            "Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
@@ -165,20 +99,8 @@ public class FrameGlassController {
             }
             frameGlassService.persist(frameGlass);
             return responseGenerator.generateOkResponse();
-        } catch(InvalidGenericProductPersistenceException e) {
-            logger.error(
-                    String.format(
-                            "There has been an error creating the frameglass [%s]",
-                            frameGlass.getUniqueName()),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. " +
-                            "Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException e) {
+            return handleUnexpectedError(e);
         }
     }
 
@@ -188,21 +110,14 @@ public class FrameGlassController {
             frameGlassService.delete(frameGlassId);
             logger.info("Deleted frameglass [" + frameGlassId + "].");
             return responseGenerator.generateOkResponse();
-        } catch (Exception e) {
-            logger.error(
-                    String.format(
-                            "There was an unexpected error trying to delete a frameglass" +
-                                    " by id [%d].",
-                            frameGlassId),
-                    e);
-            Error error = new Error(
-                    "internal-server-error",
-                    "There has been an internal server error. Please try again later.");
-            return responseGenerator.generateErrorResponse(
-                    Collections.singleton(error),
-                    HttpStatus.SERVICE_UNAVAILABLE
-            );
+        } catch (InvalidGenericProductPersistenceException e) {
+            return handleUnexpectedError(e);
         }
+    }
+
+    private ResponseEntity handleUnexpectedError(final Exception e) {
+        logger.error("There was an unexpected error while doing an operation on frames.", e);
+        return responseGenerator.generateInternalServerErrorResponse();
     }
 
 }
